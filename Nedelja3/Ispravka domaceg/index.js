3b23c410d88a6a82bb268a49968fab7b6b4252d3
StefanRale1993 <56872315+StefanRale1993@users.noreply.github.com>
2019-11-05 15:58:35 -0800

    Add files via upload

---
+++ Nedelja3/Ispravka domaceg/index.js
@@ -0,0 +1,19 @@
+//Zadatak 1
+function maxVal(a, b, c) {
+    if (a > b && a > c) {
+        return a
+    } else if (b > a && b > c) {
+        return b
+    } else if (c > b && c > a) {
+        return c
+    }
+} console.log(maxVal(46, 96, 13))
+
+
+
+
+
+
+
+
+
